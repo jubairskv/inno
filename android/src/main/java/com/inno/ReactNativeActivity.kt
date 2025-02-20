af1154887230879da2c3aaa562dfb6b05ef2d7cb
jubairk <jubairskv@gmail.com>
2025-02-20 15:11:48 +0530

    data is emitted

--- android/src/main/java/com/inno/ReactNativeActivity.kt
+++ android/src/main/java/com/inno/ReactNativeActivity.kt
@@ -4,11 +4,42 @@ import android.os.Bundle
 import android.util.Log
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import com.facebook.react.ReactApplication
 
 class ReactNativeActivity : ReactActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("ReactNativeActivity", "onCreate called")
+        
+        // Get data from intent
+        val referenceNumber = intent.getStringExtra("referenceNumber") ?: ""
+        val verificationStatus = intent.getStringExtra("verificationStatus") ?: ""
+        
+        // Emit event with verification data
+        emitVerificationEvent(referenceNumber, verificationStatus)
+    }
+
+    private fun emitVerificationEvent(referenceNumber: String, verificationStatus: String) {
+        try {
+            val params = Arguments.createMap().apply {
+                putString("referenceNumber", referenceNumber)
+                putString("verificationStatus", verificationStatus)
+            }
+
+            // Get ReactContext and emit event
+            (application as ReactApplication)
+                .reactNativeHost
+                .reactInstanceManager
+                .currentReactContext
+                ?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+                ?.emit("onReferenceIDReceived", params)
+
+            Log.d("ReactNativeActivity", "Event emitted: $referenceNumber, $verificationStatus")
+        } catch (e: Exception) {
+            Log.e("ReactNativeActivity", "Error emitting event: ${e.message}")
+        }
     }
 
     override fun getMainComponentName(): String = "Verification"
@@ -16,26 +47,10 @@ class ReactNativeActivity : ReactActivity() {
     override fun createReactActivityDelegate(): ReactActivityDelegate {
         return object : ReactActivityDelegate(this, mainComponentName) {
             override fun getLaunchOptions(): Bundle? {
-                return try {
-                    // Get data as String from Intent
-                    val referenceNumber = intent.getStringExtra("referenceNumber") ?: ""
-                    val verificationStatus = intent.getStringExtra("verificationStatus") ?: ""
-
-                    // Create props bundle
-                    val props = Bundle()
-                    props.putString("referenceNumber", referenceNumber)
-                    props.putString("verificationStatus", verificationStatus)
-
-                    // Create initial props bundle
-                    val initialProps = Bundle()
-                    initialProps.putBundle("initialProps", props)
-
-                    Log.d("ReactNativeActivitys", "Launch options created: $initialProps")
-                    initialProps
-
-                } catch (e: Exception) {
-                    Log.e("ReactNativeActivitys", "Error creating launch options", e)
-                    null
+                return Bundle().apply {
+                    putBundle("initialProps", Bundle().apply {
+                        putBoolean("isFromNative", true)
+                    })
                 }
             }
         }
