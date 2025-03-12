699e3d87e9a19436af15deed5a4c995924e7998c
jubairk <jubairskv@gmail.com>
2025-03-12 13:32:20 +0530

    React Native lifecycle issue

--- android/src/main/java/com/inno/ReactNativeActivity.kt
+++ android/src/main/java/com/inno/ReactNativeActivity.kt
@@ -60,6 +60,188 @@
 
 
 
+// package com.inno
+
+// import android.os.Bundle
+// import android.util.Log
+// import com.facebook.react.ReactActivity
+// import com.facebook.react.ReactActivityDelegate
+
+// class ReactNativeActivity : ReactActivity() {
+
+//     // Initialize with default value
+//     private var apkName: String? = "InnoExample"
+
+//     override fun onCreate(savedInstanceState: Bundle?) {
+//         // Get apkName from Intent before calling super.onCreate()
+//         apkName = intent?.getStringExtra("apkName") 
+//         super.onCreate(savedInstanceState)
+//         Log.d("ReactNativeActivityData", "Received apkName from intent: $apkName")
+//     }
+
+//     override fun getMainComponentName(): String {
+//          return apkName ?: "InnoExample"
+//     }
+    
+
+
+
+//     override fun createReactActivityDelegate(): ReactActivityDelegate {
+//         Log.d("ReactNativeActivityData", "createReactActivityDelegate called with apkName: $apkName")
+//         return object : ReactActivityDelegate(this, mainComponentName) {
+//             override fun getLaunchOptions(): Bundle? {
+//                 Log.d("ReactNativeActivityData", "getLaunchOptions called")
+//                 return try {
+//                     val sessionTimeoutStatus = intent.getIntExtra("sessionTimeoutStatus", 0)
+//                     val referenceNumber = intent.getStringExtra("referenceNumber") ?: ""
+//                     val verificationStatus = intent.getStringExtra("verificationStatus") ?: ""
+
+//                     // Create props bundle
+//                     val props = Bundle()
+//                     props.putString("referenceNumber", referenceNumber)
+//                     props.putString("verificationStatus", verificationStatus)
+//                     props.putInt("sessionTimeoutStatus", sessionTimeoutStatus)
+
+//                     // Create initial props bundle
+//                     val initialProps = Bundle()
+//                     initialProps.putBundle("initialProps", props)
+
+//                     Log.d("ReactNativeActivityData", "Launch options created: $initialProps")
+//                     initialProps
+
+//                 } catch (e: Exception) {
+//                     Log.e("ReactNativeActivityData", "Error creating launch options", e)
+//                     null
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+// package com.inno
+
+// import android.os.Bundle
+// import android.util.Log
+// import com.facebook.react.ReactActivity
+// import com.facebook.react.ReactActivityDelegate
+
+// class ReactNativeActivity : ReactActivity() {
+
+//     // Initialize with default value
+//     private var apkName: String? = "Default"
+
+//     override fun onCreate(savedInstanceState: Bundle?) {
+//         // Get apkName from Intent before calling super.onCreate()
+//         apkName = intent?.getStringExtra("apkName") ?: "Default"
+//         super.onCreate(savedInstanceState)
+//         Log.d("ReactNativeActivityData", "Received apkName from intent: $apkName")
+//     }
+
+//     override fun getMainComponentName(): String {
+//         return apkName ?: "Default"
+//     }
+
+//     override fun createReactActivityDelegate(): ReactActivityDelegate {
+//          apkName = intent?.getStringExtra("apkName") ?: "Default"
+//         Log.d("ReactNativeActivityData", "createReactActivityDelegate called with apkName: $componentName")
+//         return object : ReactActivityDelegate(this, mainComponentName) {
+//             override fun getLaunchOptions(): Bundle? {
+//                 Log.d("ReactNativeActivityData", "getLaunchOptions called")
+//                 return try {
+//                     val sessionTimeoutStatus = intent.getIntExtra("sessionTimeoutStatus", 0)
+//                     val referenceNumber = intent.getStringExtra("referenceNumber") ?: ""
+//                     val verificationStatus = intent.getStringExtra("verificationStatus") ?: ""
+
+//                     // Create props bundle
+//                     val props = Bundle()
+//                     props.putString("referenceNumber", referenceNumber)
+//                     props.putString("verificationStatus", verificationStatus)
+//                     props.putInt("sessionTimeoutStatus", sessionTimeoutStatus)
+
+//                     // Create initial props bundle
+//                     val initialProps = Bundle()
+//                     initialProps.putBundle("initialProps", props)
+
+//                     Log.d("ReactNativeActivityData", "Launch options created: $initialProps")
+//                     initialProps
+
+//                 } catch (e: Exception) {
+//                     Log.e("ReactNativeActivityData", "Error creating launch options", e)
+//                     null
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// package com.inno
+
+// import android.os.Bundle
+// import android.util.Log
+// import com.facebook.react.ReactActivity
+// import com.facebook.react.ReactActivityDelegate
+
+// class ReactNativeActivity : ReactActivity() {
+
+//     // Declare the global variable at the class level
+//     private var apkName: String? = null
+
+//     override fun onCreate(savedInstanceState: Bundle?) {
+//         Log.d("ReactNativeActivityData", "onCreate started")
+
+//         // Assign the value to the global variable inside onCreate()
+//         apkName = intent?.getStringExtra("apkName")
+//         Log.d("ReactNativeActivityData", "Received apkName from intent: $apkName")
+
+//         // Call super.onCreate() after initializing apkName
+//         super.onCreate(savedInstanceState)
+
+//         Log.d("ReactNativeActivityData", "onCreate completed")
+//     }
+
+//     override fun getMainComponentName(): String {
+//         Log.d("ReactNativeActivityData", "getMainComponentName called")
+//         return apkName ?: "Default"
+//     }
+
+//     override fun createReactActivityDelegate(): ReactActivityDelegate {
+//         Log.d("ReactNativeActivityData", "createReactActivityDelegate started")
+
+//         // Use the global apkName value
+//         val componentName = apkName ?: "Default"
+//         Log.d("ReactNativeActivityData", "createReactActivityDelegate called with apkName: $componentName")
+
+//         return object : ReactActivityDelegate(this, componentName) {
+//             override fun getLaunchOptions(): Bundle? {
+//                 Log.d("ReactNativeActivityData", "getLaunchOptions called")
+//                 return try {
+//                     val sessionTimeoutStatus = intent.getIntExtra("sessionTimeoutStatus", 0)
+//                     val referenceNumber = intent.getStringExtra("referenceNumber") ?: ""
+//                     val verificationStatus = intent.getStringExtra("verificationStatus") ?: ""
+
+//                     // Create props bundle
+//                     val props = Bundle()
+//                     props.putString("referenceNumber", referenceNumber)
+//                     props.putString("verificationStatus", verificationStatus)
+//                     props.putInt("sessionTimeoutStatus", sessionTimeoutStatus)
+
+//                     // Create initial props bundle
+//                     val initialProps = Bundle()
+//                     initialProps.putBundle("initialProps", props)
+
+//                     Log.d("ReactNativeActivityData", "Launch options created: $initialProps")
+//                     initialProps
+
+//                 } catch (e: Exception) {
+//                     Log.e("ReactNativeActivityData", "Error creating launch options", e)
+//                     null
+//                 }
+//             }
+//         }
+//     }
+// }
+
 package com.inno
 
 import android.os.Bundle
@@ -69,24 +251,36 @@ import com.facebook.react.ReactActivityDelegate
 
 class ReactNativeActivity : ReactActivity() {
 
-    // Initialize with default value
-    private var apkName: String = "DefaultActivity"
+    // Declare the global variable at the class level
+    private var apkName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Get apkName from Intent before calling super.onCreate()
-        apkName = intent?.getStringExtra("apkName") ?: "DefaultActivity"
-        super.onCreate(savedInstanceState)
+        Log.d("ReactNativeActivityData", "onCreate started")
+
+        // Assign the value to the global variable inside onCreate()
+        apkName = intent?.getStringExtra("apkName")
         Log.d("ReactNativeActivityData", "Received apkName from intent: $apkName")
+
+        // Call super.onCreate() after initializing apkName
+        super.onCreate(savedInstanceState)
+
+        Log.d("ReactNativeActivityData", "onCreate completed")
     }
 
-    override fun getMainComponentName(): String = apkName
-    
-
-
+    override fun getMainComponentName(): String {
+        Log.d("ReactNativeActivityData", "getMainComponentName called")
+        // Return the apkName if it's not null, otherwise return "Default"
+        return apkName ?: "Default"
+    }
 
     override fun createReactActivityDelegate(): ReactActivityDelegate {
-        Log.d("ReactNativeActivityData", "createReactActivityDelegate called with apkName: $apkName")
-        return object : ReactActivityDelegate(this, mainComponentName) {
+        Log.d("ReactNativeActivityData", "createReactActivityDelegate started")
+
+        // Use the value returned by getMainComponentName()
+        val componentName = mainComponentName
+        Log.d("ReactNativeActivityData", "createReactActivityDelegate called with apkName: $componentName")
+
+        return object : ReactActivityDelegate(this, componentName) {
             override fun getLaunchOptions(): Bundle? {
                 Log.d("ReactNativeActivityData", "getLaunchOptions called")
                 return try {
