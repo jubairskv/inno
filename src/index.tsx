c8160a226d5359d062085af107b157b46dd60267
jubairk <jubairskv@gmail.com>
2025-04-04 16:18:45 +0530

    Inno andriod and Ios code added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,100 @@
+// import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
+
+// const LINKING_ERROR =
+//   //The package 'react-native-inno' doesn't seem to be linked. Make sure: \n\n +
+//   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+//   '- You rebuilt the app after installing the package\n' +
+//   '- You are not using Expo Go\n';
+
+// // Platform-Specific Module Initialization
+// const SelectionActivity =
+//   Platform.OS === 'android'
+//     ? NativeModules.SelectionActivity ||
+//       new Proxy(
+//         {},
+//         {
+//           get() {
+//             throw new Error(LINKING_ERROR);
+//           },
+//         }
+//       )
+//     : null;
+
+// const Inno =
+//   Platform.OS === 'ios'
+//     ? NativeModules.Inno ||
+//       new Proxy(
+//         {},
+//         {
+//           get() {
+//             throw new Error(LINKING_ERROR);
+//           },
+//         }
+//       )
+//     : null;
+
+
+// // Create event emitters only if the corresponding modules exist
+// let iosEmitter: NativeEventEmitter | null = null;
+
+
+// if (Platform.OS === 'ios' && Inno) {
+//   try {
+//     iosEmitter = new NativeEventEmitter(Inno);
+//     console.log('iOS event emitter initialized');
+//   } catch (error) {
+//     console.error('Failed to initialize iOS event emitter:', error);
+//   }
+// }
+
+
+
+// // iOS-Specific Functions
+// export function showEkycUI(): Promise<void> {
+//   if (Platform.OS !== 'ios') {
+//     return Promise.reject('showEkycUI is only available on iOS');
+//   }
+//   return Inno.showEkycUI();
+// }
+
+// export function startLivelinessDetection(
+//   callback: (referenceID: string) => void
+// ) {
+//   if (Platform.OS !== 'ios') {
+//     throw new Error('startLivelinessDetection is only available on iOS');
+//   }
+//   if (!iosEmitter) {
+//     throw new Error('NativeEventEmitter not initialized for iOS');
+//   }
+
+//   const subscription = iosEmitter.addListener(
+//     'onReferenceIDReceived',
+//     (referenceID: string) => {
+//       console.log('Received Reference ID:', referenceID);
+//       callback(referenceID);
+//     }
+//   );
+
+//   Inno.startLivelinessDetection();
+
+//   return () => subscription.remove();
+// }
+
+// // Android-Specific Function
+// export function openSelectionScreen(referenceNumber: string , apkName : String): Promise<boolean> {
+//   if (Platform.OS !== 'android') {
+//     return Promise.reject('openSelectionScreen is only available on Android');
+//   }
+//   return SelectionActivity.openSelectionUI(referenceNumber,apkName);
+// }
+
+
+
+
 import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
 
 const LINKING_ERROR =
-  //The package 'react-native-inno' doesn't seem to be linked. Make sure: \n\n +
+  `The package 'react-native-inno' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
@@ -33,28 +126,15 @@ const Inno =
       )
     : null;
 
-
-// Create event emitters only if the corresponding modules exist
-let iosEmitter: NativeEventEmitter | null = null;
-
-
-if (Platform.OS === 'ios' && Inno) {
-  try {
-    iosEmitter = new NativeEventEmitter(Inno);
-    console.log('iOS event emitter initialized');
-  } catch (error) {
-    console.error('Failed to initialize iOS event emitter:', error);
-  }
-}
-
-
+const innoEmitter =
+  Platform.OS === 'ios' && Inno ? new NativeEventEmitter(Inno) : null;
 
 // iOS-Specific Functions
-export function showEkycUI(): Promise<void> {
+export function showEkycUI(referenceID: string): Promise<void> {
   if (Platform.OS !== 'ios') {
     return Promise.reject('showEkycUI is only available on iOS');
   }
-  return Inno.showEkycUI();
+  return Inno.showEkycUI(referenceID);
 }
 
 export function startLivelinessDetection(
@@ -63,15 +143,15 @@ export function startLivelinessDetection(
   if (Platform.OS !== 'ios') {
     throw new Error('startLivelinessDetection is only available on iOS');
   }
-  if (!iosEmitter) {
-    throw new Error('NativeEventEmitter not initialized for iOS');
+  if (!innoEmitter) {
+    throw new Error('NativeEventEmitter not initialized');
   }
 
-  const subscription = iosEmitter.addListener(
-    'onReferenceIDReceived',
-    (referenceID: string) => {
-      console.log('Received Reference ID:', referenceID);
-      callback(referenceID);
+  const subscription = innoEmitter.addListener(
+    'sessionTimeoutStatus',
+    (sessionTimeout: string) => {
+      console.log('Received Reference ID:', sessionTimeout);
+      callback(sessionTimeout);
     }
   );
 
@@ -88,3 +168,4 @@ export function openSelectionScreen(referenceNumber: string , apkName : String):
   return SelectionActivity.openSelectionUI(referenceNumber,apkName);
 }
 
+// MARK: MINE
